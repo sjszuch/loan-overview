2d875118eb3666930cdeb70b8a3c04402e39d992
Sebastian Szuch <sjszuch@gmail.com>
2023-05-26 15:23:28 -0400

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TechnologyComponent } from './components/technology/technology.component';
+import { StartscreenComponent } from './components/startscreen/startscreen.component';
+import { FlowComponent } from './components/flow/flow.component';
+import { PlanComponent } from './components/plan/plan.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: StartscreenComponent},
+  {path: 'tech', component: TechnologyComponent},
+  {path: 'flow', component: FlowComponent},
+  {path: 'plan', component: PlanComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { StartscreenComponent } from './components/startscreen/startscreen.component';
+import { TechnologyComponent } from './components/technology/technology.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PlanComponent } from './components/plan/plan.component';
+import { FlowComponent } from './components/flow/flow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    StartscreenComponent,
+    TechnologyComponent,
+    NavbarComponent,
+    PlanComponent,
+    FlowComponent
   ],
   imports: [
     BrowserModule,
